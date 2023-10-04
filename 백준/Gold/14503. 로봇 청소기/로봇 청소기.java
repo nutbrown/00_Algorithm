75febd4c7d668f6c9b8fff92a0bfac8da5c07608
junhyeryun <111677826+nutbrown@users.noreply.github.com>
2023-10-04 19:19:40 +0900

    Update 로봇 청소기.java

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -29,6 +28,7 @@ public class Main {
 		dir = sc.nextInt();
 		
 		// 각 장소 상태
+		// 1인 경우 벽이다 (테두리가 벽이 아님)
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
 				map[i][j] = sc.nextInt();
@@ -41,14 +41,11 @@ public class Main {
 		cnt = 0;
 		
 		while(true) {
-//			System.out.println(r + " " + c + " / " + dir + " / " + cnt);
-			
 			// 1. 현재 칸이 청소되지 않은 경우 청소
+			// 청소된 칸은 2로 바꿈
 			if(map[r][c] == 0) {
 				map[r][c] = 2;
 				cnt++;
-//				System.out.println("1번");
-
 			}
 			
 			// 현재 칸이 청소되었는데
@@ -61,77 +58,48 @@ public class Main {
 					
 					// 벽이 아니고 청소되어 있지 않다면
 					// isCleaned가 false다
-					if(!isWall(nr, nc) && map[nr][nc] == 0) {
+					if(map[nr][nc] != 1 && map[nr][nc] == 0) {
 						isCleaned = false;
 						break;
 					}
 				}
-//				System.out.println("isCleaned " + isCleaned);
 
 				
 				// 2. 주변 4칸 중에서 청소되지 않은 빈 칸이 없는 경우
 				if(isCleaned) {
-//					System.out.println(Arrays.toString(moveBackward(r, c)));
 					int nr = moveBackward(r, c)[0];
 					int nc = moveBackward(r, c)[1];
-//					System.out.println(nr + " " + nc + " ");
 
 					// 후진할 수 있으면
-					if(!isWall(nr, nc)){
+					if(map[nr][nc] != 1){
 						r = nr;
 						c = nc;
 					
 					// 벽이라서 후진할 수 없으면
 					} else {
-//						System.out.println("isWall ");
 						break;
 					}
-//					System.out.println("2번");
 				}
 				
 				// 3. 현재 칸 주변 칸 중 청소되지 않은 빈 칸이 있는 경우
 				else {
 					// 반시계 방향 회전하고
 					rotate();
-//					System.out.println("dir : " + dir);
 
 					// 전진했을 때 앞 칸
 					int nr = moveForward(r, c)[0];
 					int nc = moveForward(r, c)[1];
 					
 					// 앞에 있는게 벽이 아니고, 청소 안 된 거 찾을 때까지 회전
-//					System.out.println(isWall(nr, nc) + " " + map[nr][nc]);
-
-					if(!isWall(nr, nc) && map[nr][nc] == 0) {
+					if(map[nr][nc] != 1 && map[nr][nc] == 0) {
 						r = nr;
 						c = nc;
-//						System.out.println("n : " + r + " " + c + " ");
 					} 
-//					System.out.println("3번");
 				}
-
-				
 			}
 			
-//			for(int i = 0; i < N; i++) {
-//				for(int j = 0; j < M; j++) {
-//					System.out.print(map[i][j] + " ");
-//				}
-//				System.out.println();
-//			}
-//			System.out.println();
-
 		}
-		
 		System.out.println(cnt);
-	}
-	
-	
-	
-	// 해당칸이 범위 벽이 아닌지
-	private static boolean isWall(int r, int c) {
-		if(map[r][c] != 1) return false;
-		else return true;
 	}
 	
 	
@@ -156,6 +124,5 @@ public class Main {
 		else if(dir == 2) return new int[] {r + 1, c};
 		else return new int[] {r, c - 1};
 	}
-	
-	
+
 }
