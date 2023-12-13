073244f7bed7dff7b83b0df38f9bb0cebfdaaf8d
junhyeryun <111677826+nutbrown@users.noreply.github.com>
2023-12-13 16:06:06 +0900

    Update 버터 녹이기.java

--- 백준/Silver/30685. 버터 녹이기/버터 녹이기.java
+++ 백준/Silver/30685. 버터 녹이기/버터 녹이기.java
@@ -22,7 +22,7 @@ public class Main {
             // 시작은 왼쪽 오른쪽 다 x좌표
         }
 
-        // 버터 좌표로 정렬
+        // 버터 좌표 x로 정렬
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -63,35 +63,30 @@ public class Main {
         //       2 3 4 '5' 6 7 8
         // 이렇게 다양한 경우가 있어서 규칙으로 파악할 수 없음
         
-        // x1......a
-        //     b.......x2 에서 만난다면
-        // int gap = (a - b + 1) / 2
-        // a - gap까지, b + gap까지 갈 수 있다
-        // 시간은 (a - gap - x1) 아니면 (x2 - b - gap) 중에 작은 거
-        
-        // 오른쪽만 확인
+        // 앞에서부터 오른쪽만 확인
         for(int i = 0; i < N - 1; i++) {
-        	getMin(i, i + 1);
+        	getMin(i);
         }
         
+        // min이 갱신되지 않는다면 forever 가열 가능
         if(min == Integer.MAX_VALUE) System.out.println("forever");
         else System.out.println(min);
     }
     
     // 0 1 2 3 : 중앙좌표, 높이, 왼쪽경계, 오른쪽경계
-    static void getMin(int idx1, int idx2) {
-    	// 앞에 있는 오른쪽이 뒤에 있는 왼쪽 이상이라면
-    	if(arr[idx1][3] >= arr[idx2][2]) {
+    static void getMin(int idx) {
+    	// 앞(idx)에 있는 오른쪽이 뒤(idx + 1)에 있는 왼쪽 이상이라면
+    	if(arr[idx][3] >= arr[idx + 1][2]) {
     		// t를 1씩 증가시키면서 겹치기 전의 t를 구한다
     		// 이 t의 최소값을 구함
     		int t = 0;
-    		int h1 = arr[idx1][1];
-    		int right = arr[idx1][0];
-    		int h2 = arr[idx2][1];
-    		int left = arr[idx2][0];
+    		int h1 = arr[idx][1];
+    		int right = arr[idx][0];
+    		int h2 = arr[idx + 1][1];
+    		int left = arr[idx + 1][0];
     		
-    		while(right < left) {
-    			// 시간이 증가하면 한 칸씩 좁혀짐
+    		while(true) {
+    			// 한 칸 좁히고
     			t++;
     			if(h1 > 1) {
     				h1 -= 2;
@@ -101,10 +96,13 @@ public class Main {
     				h2 -= 2;
     				left--;
     			}
+    			// 좁혔더니 겹치면 최소값 갱신하고 아웃
+    			if(right >= left) {
+    				min = Math.min(min, t - 1);
+    				break;
+    			}
     		}
-    		// 이미 겹쳐서 while문을 돌지 않고 min은 t - 1
-    		min = Math.min(min, t - 1);
+    		
     	}
     }
-    
 }
