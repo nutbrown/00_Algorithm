ff79e3e3064e204af4486c131e642ba627fb932e
junhyeryun <111677826+nutbrown@users.noreply.github.com>
2024-01-07 04:09:36 +0900

    Title: 세 수의 합, Time: 532 ms, Memory: 35780 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,19 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		// 숫자 N개 입력
-		int N = Integer.parseInt(br.readLine());
+		int N = sc.nextInt();
 		int[] arr = new int[N];
 		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = sc.nextInt();
 		}
 		
 		// 숫자 리스트 정렬
