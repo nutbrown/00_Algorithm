7d79d6f0945b89b2850a7a4e72d12526b6282647
junhyeryun <111677826+nutbrown@users.noreply.github.com>
2024-01-31 04:06:51 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 232 ms, Memory: 18456 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.java
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.java
@@ -8,42 +8,44 @@ public class Main {
 		int N = sc.nextInt();
 		String S = sc.next();
 		
-		ArrayList<Integer> num = new ArrayList<>();
+		int[] num = new int[N];
 		for(int i = 0; i < S.length(); i++) {
-			num.add(Character.getNumericValue(S.charAt(i)));
+			num[i] = Character.getNumericValue(S.charAt(i));
 		}
 		
 		// 1일이상 641일이하 가능
 		// 3자리 수가 가능하면 무조건 3자리 수
 		// 6이하여야함 + 
 		
+		// 병사수
 		int cnt = 0;
-		while(!num.isEmpty()) {
+		// 현재 전역일 시작 인덱스
+		int idx = 0;
+		
+		while(idx < N) {
 			boolean flag = false;
-			if(num.size() >= 3) {
+			// 0 1 2 3 [4]
+			// 3자리 없애기
+			if(N - idx >= 3) {
 				boolean flag2 = true;
-				if(num.size() > 3 && num.get(3) == 0) {
+				// 3개를 없앴을 때 그 다음 숫자가 있는데, 0이면 안 된다
+				if(N - idx > 3 && num[idx + 3] == 0) {
 					flag2 = false;
 				}
-				if(flag2 && num.get(0) < 6) {
+				
+				else if(flag2 && num[idx] < 6) {
 					cnt++;
-					num.remove(0);
-					num.remove(0);
-					num.remove(0);
+					idx += 3;
 					flag = true;
-				} else if(num.get(0) == 6) {
-					if(num.get(1) < 4) {
+				} else if(num[idx] == 6) {
+					if(num[idx + 1] < 4) {
 						cnt++;
-						num.remove(0);
-						num.remove(0);
-						num.remove(0);
+						idx += 3;
 						flag = true;
-					} else if(num.get(1) == 4) {
-						if(num.get(2) <= 1) {
+					} else if(num[idx + 1] == 4) {
+						if(num[idx + 2] <= 1) {
 							cnt++;
-							num.remove(0);
-							num.remove(0);
-							num.remove(0);
+							idx += 3;
 							flag = true;
 						}
 					}
@@ -53,15 +55,15 @@ public class Main {
 			// 3자리 없앴으면 다음 3자리 찾기
 			if(flag) continue;
 
-			if(num.size() >= 2) {
+			if(N - idx >= 2) {
 				boolean flag2 = true;
-				if(num.size() > 2 && num.get(2) == 0) {
+				// 2개를 없앴을 때 다음 숫자가 있는데, 0이면 안 된다
+				if(N - idx > 2 && num[idx + 2] == 0) {
 					flag2 = false;
 				}
-				if(flag2) {
+				else if(flag2) {
 					cnt++;
-					num.remove(0);
-					num.remove(0);
+					idx += 2;
 					flag = true;
 				}
 			}
@@ -69,10 +71,9 @@ public class Main {
 			// 2자리 없앴으면 다음 2자리 찾기
 			if(flag) continue;
 			
-			if(num.size() >= 1) {
-				cnt++;
-				num.remove(0);
-			}
+			// 3자리 2자리 다 안 되면 1자리 없애기
+			cnt++;
+			idx++;
 		}
 		
 		System.out.println(cnt);
